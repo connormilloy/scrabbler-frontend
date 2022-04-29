771e5140d97a805ad3839ae39e7640c0d72355de
connormilloy <74675162+connormilloy@users.noreply.github.com>
2022-04-29 17:16:13 +0100

    Obscene amount of local work done before initial commit

---
+++ src/Components/InputBox/InputBox.js
@@ -0,0 +1,11 @@
+import styles from './InputBox.module.scss';
+
+const InputBox = () => {
+    return(
+        <div className="inputBox">
+
+        </div>
+    )
+}
+
+export default InputBox;
---
+++ src/Components/InvalidWord/InvalidWord.js
@@ -0,0 +1,11 @@
+import styles from './InvalidWord.module.scss';
+
+const InvalidWord = () => {
+    return(
+        <div className={styles.invalidWord}>
+            <h4>Invalid Word!</h4>
+        </div>
+    )
+}
+
+export default InvalidWord;
